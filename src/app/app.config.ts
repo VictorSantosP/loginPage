35ba79ebf28e3cf7f9a96c1223924e570584070a
Victor Santos <vhdboss@gmail.com>
2026-09-18 02:02:24 -0300

    feat: create login page

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,18 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideToastr } from '@iqx-limited/ngx-toastr';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(),
+    provideToastr({
+      positionClass: 'toast-top-center',
+      timeOut: 10000,
+    }),
   ]
 };
---
+++ src/app/services/login.spec.ts
@@ -0,0 +1,15 @@
+import { TestBed } from '@angular/core/testing';
+import { LoginService } from './loginService';
+
+describe('Login', () => {
+  let service: LoginService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(LoginService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
